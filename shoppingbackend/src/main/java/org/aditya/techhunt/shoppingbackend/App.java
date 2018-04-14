d24bf6bd1ee2568cc24895c87f40926771e69cc0
adityasrivastva <aditya5srivastva@gmail.com>
2018-04-14 11:09:53 +0530

    Ecart base version

---
+++ shoppingbackend/src/main/java/org/aditya/techhunt/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package org.aditya.techhunt.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
